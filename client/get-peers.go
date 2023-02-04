eb4a39c602cebfd2a93c8e0ee479207b9e6b27d6
hello_world_was_taken <haileformal@gmail.com>
2023-02-04 20:01:08 +0300

    refactor get-peers.go

--- client/get-peers.go
+++ client/get-peers.go
@@ -1,64 +1,104 @@
 package client
 
-import "torrent-dsp/model"
-
 import (
-	"net"
+	"fmt"
+	"net/http"
+	"net/url"
+	"torrent-dsp/model"
+	"torrent-dsp/utils"
 
+	bencode "github.com/zeebo/bencode"
 )
 
-func GetPeersFromTracker(torrent *model.Torrent, conn net.Conn) ([]*model.Peer, error) {
+// Get peers from one of the trackers in the torrent file
+func GetPeersFromTrackers(torrent *model.Torrent) ([]*model.Peer, error) {
 	// create a request to send to the tracker
-	request, err := CreateTrackerRequest(torrent)
+	httpTrackerURLs, err := buildTrackerRequestURLs(torrent)
 	if err != nil {
 		return nil, err
 	}
 
-	// send the request to the tracker
-	err = SendTrackerRequest(request, conn)
+	go getPeersFromTrackersHelper(httpTrackerURLs)
 	if err != nil {
 		return nil, err
 	}
-
-	// get the response from the tracker
-	// response, err := GetTrackerResponse(conn)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// // parse the response and get a list of peers
-	// peers, err := ParseTrackerResponse(response)
-	// if err != nil {
-	// 	return nil, err
-	// }
 
 	// return peers, nil
 	return nil, nil
 }
 
-func CreateTrackerRequest(torrent *model.Torrent) (*model.TrackerRequest, error) {
-	// create a new tracker request
-	request := model.TrackerRequest{
+
+// builds a tracker request url from the announcement list
+func buildTrackerRequestURLs(torrent *model.Torrent) ([]string, error) {
+	// query parameters to the tracker url
+	requestParams := model.TrackerRequestParams{
 		Info_hash:  torrent.InfoHash,
-		Peer_id:    "-TR2770-6x6x6x6x6x6x",
-		Port:       6889,
+		Peer_id:    "-TR2940-k8hj0wgej6ch",
+		Port:       6881,
 		Uploaded:   0,
 		Downloaded: 0,
 		Left:       torrent.Info.Length,
 		Event:      "started",
 	}
 
-	return &request, nil
+	// list of urls
+	URLs := []string{}
+
+	// go through all the trackers in the announce list and create a request for each one
+	// discard udp and only take http trackers
+	for _, tracker := range torrent.AnnounceList {
+		// parse the tracker url
+		URL, err := url.Parse(tracker[0])
+		if err != nil {
+			return []string{}, err
+		}
+
+		if utils.IsHTTPTracker(URL.String()) {
+			// add the query parameters to the tracker url
+			URL.RawQuery = requestParams.Encode()
+			URLs = append(URLs, URL.String())
+		}
+	}
+
+	return URLs, nil
 }
 
 
-func SendTrackerRequest(request *model.TrackerRequest, conn net.Conn) error {
-	// send the request to the tracker
-	_, err := conn.Write([]byte(request.Encode()))
-	if err != nil {
-		return err
+// go through each HTTP tracker requests until we get a response from one
+func getPeersFromTrackersHelper(URLs []string) (model.TrackerResponse, error) {
+
+	for _, URL := range URLs {
+		response, err := getPeerFromURL(URL)
+		if err == nil {
+			return response, nil
+		}
+
+		fmt.Println("Error getting peers from HTTP tracker: ", err)
 	}
 
-	return nil
+	return model.TrackerResponse{}, nil
+}
+
+
+// try to get peers from a passed in tracker request url
+func getPeerFromURL(URL string) (model.TrackerResponse, error) {
+	// send the request to the tracker
+	response, err := http.Get(URL)
+	if err != nil {
+		return model.TrackerResponse{}, err
+	}
+	
+	// close the response body
+	defer response.Body.Close()
+
+	// decode the response
+	trackerResponse := model.TrackerResponse{}
+	err = bencode.NewDecoder(response.Body).Decode(&trackerResponse)
+	if err != nil {
+		return model.TrackerResponse{}, err
+	}
+
+	fmt.Println("Retrieved peers successfully")
+	return trackerResponse, nil
 }
 
