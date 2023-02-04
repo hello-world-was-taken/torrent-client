62b9fa759a27e2f1a32551604d041e5da417d26d
hello_world_was_taken <haileformal@gmail.com>
2023-02-04 20:02:23 +0300

    add get-peers feature

--- client/client.go
+++ client/client.go
@@ -2,34 +2,20 @@
 package client
 
 import (
-	"fmt"
 	"log"
-	// "net"
+	
 	"torrent-dsp/utils"
-
-	// "github.com/anacrolix/torrent"
 )
 
 
 func ConnectToTracker() {
 	// open torrent file from the current directory and parse it
-	torrent, err := utils.ParseTorrentFile("./torrent-files/ubuntu-14.04-desktop-amd64+mac.iso.torrent")
+	torrent, err := utils.ParseTorrentFile("./torrent-files/ubuntu-22.10-desktop-amd64.iso.torrent")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// log print torrent file
-	// utils.LogTorrent(torrent)
-	fmt.Println(">>", torrent.InfoHash)
-
-	// connect to tracker from torrent file
-	// conn, err := net.Dial("tcp", torrent.Announce)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer conn.Close()
-
-	// // get a list of peers from the connected tracker
-	// peers, err := GetPeersFromTracker(torrent, conn)
+	// get a list of peers from the tracker
+	_, err = GetPeersFromTrackers(&torrent)
 
 }
--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
-import (
-	// "fmt"
-
-	"torrent-dsp/client"
-	// "torrent-dsp/utils"
-	// "torrent-dsp/server"
-)
+import "torrent-dsp/client"
 
 func main() {
-	// go server.StartTracker()
 	client.ConnectToTracker()
-	// fmt.Println(utils.GetLocalIP())
 
 	// infinite loop to keep the program running
 	for { }
