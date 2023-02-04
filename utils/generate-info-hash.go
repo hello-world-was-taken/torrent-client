3f0e4794f0b88659696c7eb73b8f161d26b997b0
hello_world_was_taken <haileformal@gmail.com>
2023-02-04 20:01:38 +0300

    add info_hash generation function

--- utils/generate-info-hash.go
+++ utils/generate-info-hash.go
@@ -6,21 +6,19 @@ import (
 	"log"
 
 	"torrent-dsp/model"
+
 	bencode "github.com/zeebo/bencode"
 )
 
-func GenerateInfoHash(info model.Info) string {
+func GenerateInfoHash(info model.Info) [20]byte {
 
 	// encode the info back to bencode format
 	encodedInfo, err := bencode.EncodeBytes(info)
 	if err != nil {
-		fmt.Print("Encountered error while encoding info")
 		log.Fatal(err)
 	}
 	
 	// generate sha1 hash from the encoded info
 	hash := sha1.Sum(encodedInfo)
-	r := fmt.Sprintf("%x", hash)
-	fmt.Println("Hash: ", r)
-	return r
+	return hash
 }
