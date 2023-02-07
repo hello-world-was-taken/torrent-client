5dc4493730fd0951a62c731dd3ff43a74018911c
hello_world_was_taken <haileformal@gmail.com>
2023-02-07 04:41:39 +0300

    add string function to peer

--- model/peer.go
+++ model/peer.go
@@ -1,8 +1,15 @@
 package model
 
-import "net"
+import (
+	"net"
+	"strconv"
+)
 
 type Peer struct {
 	IP   net.IP `bencode:"ip"`
 	Port uint16   `bencode:"port"`
+}
+
+func (peer *Peer) String() string {
+	return peer.IP.String() + ":" + strconv.Itoa(int(peer.Port))
 }
