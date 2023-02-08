3ef37481d218cba7d2553a02945bb482bd8117ad
hello_world_was_taken <haileformal@gmail.com>
2023-02-08 07:37:26 +0300

    add handshake struct

--- model/hand-shake-request.go
+++ model/hand-shake-request.go
@@ -1,16 +1,21 @@
 package model
 
+import (
+	"fmt"
+	"net"
+)
+
 type HandShake struct {
-	Pstr     string
-	InfoHash [20]byte
-	PeerID   [20]byte
+	Pstr     string   `bencode:"pstr"`
+	InfoHash [20]byte `bencode:"info_hash"`
+	PeerID   [20]byte `bencode:"peer_id"` 
 }
 
 // serialize the handshake request
 // <pstrlen 1 byte><pstr 19 bytes i.e Bit Torrent><reserved 8 byte><info_hash><peer_id 20 bytes>
 func (handShake *HandShake) Serialize() []byte {
 	// create a buffer to store the handshake request. For bittorrent, request length is 49 + len(pstr) ==> 49 + 19 = 68
-	buffer := make([]byte, 49 + len(handShake.Pstr))
+	buffer := make([]byte, 49+len(handShake.Pstr))
 	buffer[0] = byte(len(handShake.Pstr))
 	copy(buffer[1:], []byte(handShake.Pstr))
 	copy(buffer[1+len(handShake.Pstr):], make([]byte, 8))
@@ -20,14 +25,40 @@ func (handShake *HandShake) Serialize() []byte {
 	return buffer
 }
 
-
 // deserialize the handshake response byte array into a HandShake struct
 func DeserializeHandShake(buffer []byte) *HandShake {
 	handShake := &HandShake{}
 	pstrLength := int(buffer[0])
-	handShake.Pstr = string(buffer[1:pstrLength+1])
-	copy(handShake.InfoHash[:], buffer[pstrLength+28:pstrLength+48])
-	copy(handShake.PeerID[:], buffer[pstrLength+48:pstrLength+68])
+	handShake.Pstr = string(buffer[1 : pstrLength+1])
+	// TODO: remove magic numbers
+	copy(handShake.InfoHash[:], buffer[28:48])
+	copy(handShake.PeerID[:], buffer[48:68])
 
 	return handShake
+}
+
+func (h *HandShake) Send(conn net.Conn) (*HandShake, error) {
+	// serialize the handshake request
+	buffer := h.Serialize()
+
+	// send the handshake request
+	_, err := conn.Write(buffer)
+	if err != nil {
+		return &HandShake{}, err
+	}
+
+	// read the handshake response
+	// handshake response is 68 bytes long <length of Protocol> + <Protocol> + <Reserved> + <InfoHash> + <PeerID>
+	buffer = make([]byte, 68)
+	_, err = conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading handshake response")
+		return &HandShake{}, err
+	}
+
+	// deserialize the handshake response
+	handShake := DeserializeHandShake(buffer)
+
+	fmt.Println("Handshake sent successfully")
+	return handShake, nil
 }
