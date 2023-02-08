590cb5a384b14c9d9f1551c1b2d64e9add5f0f6b
hello_world_was_taken <haileformal@gmail.com>
2023-02-08 07:39:36 +0300

    fix overflow bug while deserializing handshake response

--- client/client.go
+++ client/client.go
@@ -1,10 +1,13 @@
-// dial to localhost:6889 and send a message
 package client
 
 import (
 	"log"
-	
+	"net"
+	"fmt"
+
 	"torrent-dsp/utils"
+	"torrent-dsp/model"
+	"torrent-dsp/constant"
 )
 
 
@@ -13,12 +16,41 @@ func ConnectToTracker() {
 	// TODO: remove this hardcoded torrent file names
 	// ubuntu-22.10-desktop-amd64.iso
 	// vlc-media-player
-	torrent, err := utils.ParseTorrentFile("./torrent-files/vlc-media-player.torrent")
+	torrent, err := utils.ParseTorrentFile("./torrent-files/ubuntu-22.10-desktop-amd64.iso.torrent")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// get a list of peers from the tracker
-	_, err = GetPeersFromTrackers(&torrent)
+	peers, err := GetPeersFromTrackers(&torrent)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// log the peers
+	// utils.LogPeers(peers)
+
+	// connect to the peers
+	fmt.Println("Connecting to peers...")
+	ConnectToPeers(peers, torrent)
+
+}
+
+
+// connect to the peers
+func ConnectToPeers(peers []model.Peer, torrent model.Torrent) {
+	for _, peer := range peers {
+		// create a new connection to the peer
+		conn, err := net.DialTimeout("tcp", peer.String(), constant.CONNECTION_TIMEOUT)
+		if err != nil {
+			fmt.Println("Error connecting to peer: ", peer.String())
+			// log.Fatal(err)
+		}
+		
+		if err == nil {
+			// shake hands with the peer
+			ShakeHandWithPeer(torrent, peer, constant.CLIENT_ID, conn)
+		}
+		
+	}
 }
