99e16ac632c9ab82a1dd2e4b4ec9f58413041aea
hello_world_was_taken <haileformal@gmail.com>
2023-02-19 08:35:01 +0300

    add bit length check

--- utils/bit-manipulation.go
+++ utils/bit-manipulation.go
@@ -6,6 +6,11 @@ func BitOn(byteField []byte, bitIdx int) bool {
 	bitIdxOnCurrByte := bitIdx % 8
 
 	// on bit means it has the piece data
+	// check if the length of the bit field is greater than the byte index
+	// if not, then the peer does not have the piece
+	if len(byteField) <= byteIdx {
+		return false
+	}
 	return byteField[byteIdx] & (1 << bitIdxOnCurrByte) != 0
 }
 
