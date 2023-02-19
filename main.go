7c66bbe6eb77d14a6a6fc8ca40eae01eb02788eb
hello_world_was_taken <haileformal@gmail.com>
2023-02-19 08:51:20 +0300

    remove infinite loop to keep the program running

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "torrent-dsp/client"
 
 func main() {
+	// client.StartDownload()
 	client.ConnectToTracker()
-
 	// infinite loop to keep the program running
-	for { }
+	// for { }
 }
