d7202cc04682fd2f9cde5e79c0464520166370a6
hello_world_was_taken <haileformal@gmail.com>
2023-02-22 02:00:04 +0300

    add piece hash checker

---
+++ utils/bit-hash-checker.go
@@ -0,0 +1,12 @@
+package utils
+
+import (
+	"bytes"
+	"crypto/sha1"
+)
+
+
+func BitHashChecker(buf []byte, pieceHash [20]byte) bool {
+	hash := sha1.Sum(buf)
+	return bytes.Equal(hash[:], pieceHash[:])
+}
