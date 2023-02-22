0671371f5d0f30bad2070811ea04d452eb757f78
hello_world_was_taken <haileformal@gmail.com>
2023-02-22 12:18:37 +0300

    fix file truncate bug

--- common/multiple-file-handler.go
+++ common/multiple-file-handler.go
@@ -8,7 +8,7 @@ import (
 	"torrent-dsp/model"
 )
 
-func CreateFile(torrent *model.Torrent) (*os.File, error) {
+func CreateFile(torrent *model.Torrent) (*os.File, string, error) {
 	fmt.Println("Creating folder structure for", torrent.Info.Name)
 
 	// create the folder structure
@@ -18,7 +18,7 @@ func CreateFile(torrent *model.Torrent) (*os.File, error) {
 			// Get the absolute path of the file
 			absPath, err := filepath.Abs("downloads" + "/" + torrent.Info.Name + "/" + filePath.Path[0])
 			if err != nil {
-				return nil, err
+				return nil, "", err
 			}
 		
 			// Get the directory path of the file
@@ -26,29 +26,57 @@ func CreateFile(torrent *model.Torrent) (*os.File, error) {
 		
 			// Create the directory path if it doesn't exist
 			if err := os.MkdirAll(dirPath, os.ModePerm); err != nil {
-				return nil, err
+				return nil, "", err
 			}
 		
 			// Create the file
 			fmt.Println("Creating file", idx)
 			file, err := os.Create(absPath)
 			if err != nil {
-				return nil, err
+				return nil,"", err
 			}
 			defer file.Close()
 		}
 
-		outFile, err := os.Create("downloads" + "/" + torrent.Info.Name + ".temp")
+		outFile, err := CreateOrOpenFile("downloads" + "/" + torrent.Info.Name + ".temp")
 		if err != nil {
-			return nil, err
+			return nil, "", err
 		}
-		return outFile, err
+		return outFile, "", err
 	} else {
 		// single file
 		outFile, err := os.Create("downloads" + "/" + torrent.Info.Name)
 		if err != nil {
+			return nil, "", err
+		}
+		return outFile, "", err
+	}
+}
+
+
+func CreateOrOpenFile(filename string) (*os.File, error) {
+	// load the cache from a file
+	_, err := os.Stat(filename)
+	if err != nil && !os.IsNotExist(err) {
+		fmt.Println("error different from nil and err != from not exist")
+		return nil, err
+	}
+
+	// if the error is that the file does not exist, then create a new cache
+	if os.IsNotExist(err) {
+		file, err := os.Create(filename)
+		if err != nil {
+			fmt.Println("Error while creating file", filename)
 			return nil, err
 		}
-		return outFile, err
+		return file, nil
 	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error opening file")
+		return nil, err
+	}
+
+	return file, nil
 }
